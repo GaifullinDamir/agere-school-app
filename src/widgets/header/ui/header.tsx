228122c6d18ac4d12737af474974b8f42538f15b
Damir <gaifullindr@gmail.com>
2024-03-09 17:20:18 +0300

    Реализован базовый header.

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -4,15 +4,17 @@ const Header = () => {
     return (
         <header className='header'>
             <div className='header__logo'><span>Agere</span></div>
-            <ul className='header__menu'>
-                <li className='header__menu-item'>Каталог</li>
-                <li className='header__menu-item'>Обучение</li>
-                <li className='header__menu-item'>Преподавание</li>
-            </ul>
-            <img className='header__theme-icon' src={moonIcon}/>
-            <div className='header__user'>
-                <span className='header__username'>Name Surname</span>
-                <img className='header__userimg' src={userIcon}/>
+            <div className='header__container'>
+                <ul className='header__menu'>
+                    <li className='header__menu-item'>Каталог</li>
+                    <li className='header__menu-item'>Обучение</li>
+                    <li className='header__menu-item'>Преподавание</li>
+                </ul>
+                <div className='header__user-panel'>
+                    <img className='header__theme-icon' src={moonIcon}/>
+                    <span className='header__username'>Name Surn...</span>
+                    <img className='header__userimg' src={userIcon}/>
+                </div>
             </div>
         </header>
     );
